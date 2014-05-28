3792f602c497b6747e08ab759b0855f46f76aa8e
Dilawar <dilawars@iitb.ac.in>
2014-05-28 08:06:00 +0530

    I can now generate action potential.

--- ActionPotential/diff_ap.hs
+++ ActionPotential/diff_ap.hs
@@ -12,18 +12,27 @@ nullAP = ActionPotential { time = [], volt = [] }
 
 -- Get action potential from a file. 
 {-getAP :: String -> [ ActionPotential ]-}
-getAP txt = createTables $ map pairUp values
+getAP txt = map tableToActionPotential tables
     where 
-        values = map (\x -> map (\y -> read y :: Float) $ splitOneOf " ,;" x) lines
-        lines = splitOn "\n" txt
+        tables = createTables $ map pairUp values
+        values = map (\x -> map (\y -> read y :: Double) $ splitOneOf " ,;" x) lines
+        lines = filter (/=[]) $ splitOn "\n" txt
 
+-- Given a table like structure, convert it inot ActionPotential
+tableToActionPotential table = ActionPotential{ time = fst unzipped
+    , volt = snd unzipped
+    }
+    where
+        unzipped = unzip table 
+
+-- GIven a list of values, create a table like structure.
 createTables vs = foldl intoTables' emptyTables vs
     where
         emptyTables = map (\y -> [] ) (head vs)
         intoTables' [] [] = []
         intoTables' (c:cs) (a:as) = (c ++ [a]) : intoTables' cs as
         -- Sometime we might get an empty list as element. Ignore it
-        intoTables' x [] = x
+        -- intoTables' x [] = x
 
 -- Pair up first element of list with rest of the list. For example, 
 -- pairUp [1, 2, 3] = [ (1, 2), (1, 3) ]
