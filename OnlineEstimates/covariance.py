f614ea2efa54830d57bd983b61510abcbf2edb59
Dilawar Singh <dilawars@ncbs.res.in>
2015-06-10 10:49:00 +0530

    Another data set with small std and mean 0.

--- OnlineEstimates/covariance.py
+++ OnlineEstimates/covariance.py
@@ -33,7 +33,8 @@ def online_estimate(data):
 
 def main():
     #data = np.random.sample(1e6)
-    data = np.random.random_integers(0, 1000, 1e6)
+    #data = np.random.random_integers(0, 1000, 1e6)
+    data = np.random.normal(0.0, 1e-5, 1e6)
     variance = online_estimate(data)
     print("Online estimate is: %s" % variance)
     variance = data.var()
