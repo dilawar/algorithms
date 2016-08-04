3c37112a712a1536e36d975242c9f93f311d8a36
Dilawar Singh <dilawars@ncbs.res.in>
2016-08-04 14:50:42 +0530

    Make it terminal based script.

---
+++ Statistics/MarkovChains/markov.py
@@ -0,0 +1,88 @@
+"""markov.py: 
+
+Class for Markov Process
+
+"""
+import __future__
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2016, Dilawar Singh"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class MarkovChain():
+    """Markov chains"""
+
+    def __init__(self, T, **kwargs):
+        self.T = None
+        self.init_transition_matrix( T )
+        # size of the system
+        self.N = self.T.shape[0]
+
+    def init_transition_matrix( self, mat_or_string ):
+        if isinstance( mat_or_string, str ):
+            rows = []
+            rs = mat_or_string.split( ';' )
+            for r in rs:
+                # Trick to avoid 1/3 evaluated to 0
+                r = [ eval( compile(
+                    x, '<string>', 'eval', __future__.division.compiler_flag
+                    )) for x in r.split( ) ]
+                rows.append( r )
+            self.T = np.vstack( rows )
+        else:
+            self.T = np.matrix( mat_or_string )
+
+        self.N = self.T.shape[0]
+        assert np.sum( self.T ) == self.N, 'Invalid transition matrix'
+        assert np.allclose( np.sum( self.T , axis = 1), np.ones( self.N )), 'Invalid transition matrix'
+
+    def find_steady_state( self, method = 'analytic',  max_iterations = 1000 ):
+        # There are two ways in which one can do that. 
+        if method == 'numerical':
+            oldT = self.T
+            newT = np.dot(oldT,  self.T)
+            i = 0
+            while not np.allclose( newT, oldT ):
+                oldT = newT
+                newT = np.dot(oldT,  self.T)
+                i += 0
+                assert i < max_iterations, 'Increase the max_iterations'
+            return newT.diagonal( )
+
+        # Other method is to use the steady state argument that 
+        #    finalT = finalT * self.T
+        #    self.T' * finalT' = finalT' (A' = transpose A)
+        a = self.T.T - np.identity( self.T.shape[0] )
+        b = np.zeros( self.T.shape[0] )
+
+        a[self.N-1,:] = np.ones( self.N )
+        b[self.N-1] = 1
+        
+        # Up to now, this system is lineary dependant. Use that fact all
+        # probabilities sums up to 1.
+        return np.linalg.solve( a, b )
+
+
+def main( ):
+    # mc = MarkovChain( '0 1/3 2/3; 0 0 1; 1 0 0' )
+    mc = MarkovChain( '0.9 0.075 0.025;0.15 0.8 0.05;0.25 0.25 0.5' )
+    print mc.find_steady_state( )
+
+
+if __name__ == '__main__':
+    main()
+
+
+        
