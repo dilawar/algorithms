3c5d1f042df9db49075dc55ed744b1d216b8fcc9
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-18 10:49:22 +0530

    Okay. Its can generate the cluster.

--- GraphTheory/test_clustering.py
+++ GraphTheory/test_clustering.py
@@ -22,6 +22,11 @@ import numpy as np
 import networkx as nx
 import random
 
+random.seed( 10 )
+np.random.seed( 10 )
+
+colors_ = []
+
 def filter_graph( g, min_weight = 1.0 ):
     toRemove = [ ]
     for s, t in g.edges( ):
@@ -32,28 +37,61 @@ def filter_graph( g, min_weight = 1.0 ):
     print( 'Total edges after filtering %s' % g.number_of_edges( ) )
     return g
 
-def cluster_graph( g ):
-    # Start with the node with maximum degree
+def cluster_graph( g, colors, current_color = 0 ):
+    # Initialize colors
+    degrees = []
+    for n in g.nodes( ):
+        degrees.append( (nx.degree(g, n ), n))
+
+    nodeWithMaxDegree = sorted( degrees, reverse = True )[0][1]
+
+    # Start with node with maximum degree
+    current_color  += 1
+    colors[ nodeWithMaxDegree ] = current_color
+
+    numIteration = 0
+    pN = nodeWithMaxDegree
+    stack = [ nodeWithMaxDegree ]
+    while stack:
+        neighbours = nx.neighbors( g, stack.pop( ) )
+        numIteration += 1
+        for n in neighbours:
+            if colors[n] > 0:
+                continue
+            # If most neightbours of n are as close to pN as the value current
+            # iteration, then accept this node.
+            potentialCandidates = filter(lambda x: colors[x] == 0, nx.neighbors( g, n ))
+            # print( "Potential neighbours ", potentialCandidates )
+            distance2pN = [ nx.shortest_path_length( g, x, pN) for x in potentialCandidates ]
+            goodDist2pN = filter( lambda x: x <= numIteration, distance2pN )
+            accepted = False
+            if len( goodDist2pN ) > 0:
+                accepted = True
+                colors[ n ] = current_color 
+                stack.append( n )
+            # print n, ' -> ', distance2pN, ',', goodDist2pN, 'Accepted', accepted
+        # print stack
     return g
 
 def main( ):
-    g = nx.complete_graph( 50 )
+    g = nx.complete_graph( 20 )
     for s, t in g.edges( ):
-        g[s][t]['weight'] = (0.1 + random.random( )) ** 2
+        g[s][t]['weight'] = (0.15 + random.random( )) ** 2
 
-    plt.subplot( 121 )
+    plt.subplot( 211 )
     g = filter_graph( g )
 
     width = []
     for s, t in g.edges( ):
         width.append( g[s][t]['weight'] ** 2.0 )
-
     nx.draw_networkx( g, width = width )
 
-    plt.subplot( 122 )
+    plt.subplot( 212 )
     # Now do the thingy
-    # nx.draw( g )
-    cluster_graph( g )
+    for n in g.nodes( ):
+        colors_.append( 0 )
+    g = cluster_graph( g, colors_, 0 )
+    nx.draw_networkx( g, node_color = colors_ )
     # print( cluster )
     plt.savefig( 'test_graph.png' )
 
