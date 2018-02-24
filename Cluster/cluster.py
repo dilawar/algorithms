b8cc9ab90c95243cee8c370992a596abd02d5fe4
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-24 18:25:35 +0530

    Not working.

--- Cluster/cluster.py
+++ Cluster/cluster.py
@@ -17,56 +17,6 @@ import networkx as nx
 import numpy as np
 import re
 
-def build_flow_graph( vec, n ):
-    g = nx.DiGraph( )
-    g.add_node( 'source' )
-    g.add_node( 'sink' )
-
-    leaf = [ ]
-    for i in range( 0, int(len(vec)/n)):
-        g.add_node( 'chunk.%d' % i )
-        g.add_edge( 'source', 'chunk.%d'%i, weight = 0, capacity = 1 )
-        xs = vec[i*n:(i+1)*n]
-
-        added = [ ]
-        for ii, x in enumerate(xs):
-            if x in added:
-                continue
-            else:
-                added.append( x )
-
-            name = '%d.%s' % (i*n+ii,x)
-            g.add_node( name )
-            g.add_edge( 'chunk.%d' % i, name, capacity=n, weight = 0 )
-            leaf.append( name )
-
-    # each leaf connect to same x 
-    for l in leaf:
-        index, val = l.split( '.' )
-        index = int( index )
-        for j, y in enumerate( vec ):
-            if j == index:
-                continue
-            if val != y:
-                continue
-            ln = 'L.%d.%s' % (j,y)
-            g.add_edge( l, ln, capacity = 1, weight = abs(index-j) )
-            g.add_edge( ln, 'sink', capacity = 1, weight = 0 )
-
-    return g
-
-def compute_solution( g ):
-    flowG = nx.DiGraph( )
-    flowDict = nx.max_flow_min_cost( g, 'source', 'sink' )
-    for n in flowDict:
-        for m in flowDict[n]:
-            flow = flowDict[n][m]
-            if flow == 1:
-                print(n, m, flow )
-                flowG.add_edge( n, m )
-
-    write_graph( flowG, 'flow.dot' )
-
 def write_graph( g, outfile ):
     from networkx.drawing.nx_agraph import write_dot
     write_dot( g, outfile )
@@ -87,12 +37,17 @@ def find_groups( string, alphas, n ):
     return pat.findall( string )
 
 def cost_of_moving( d ):
-    return d if d > 0 else d ** 2
+    # Positive d means postponing AWS or moving to right.
+    return d+d if d > 0 else d**2 + abs(d)
 
-def print_result( res ):
+def cost_of_moving_elem( v, newpos ):
+    return cost_of_moving( newpos - v[1] )
+
+def print_result( res, indent = 2 ):
+    prefix = ''.join([ ' ' for i in range( indent ) ] )
     s = ''.join( [ x[0] for x in res ] )
     ks = ''.join( [ '%s' % x[2]['k'] for x in res ] )
-    print( s + "\n" + ks )
+    print( prefix + s + "\n" + prefix + ks )
 
 def find_same_neighbour( i, c, values ):
     res = [ ]
@@ -101,6 +56,7 @@ def find_same_neighbour( i, c, values ):
             res.append( i+j+1 )
         else:
             break
+
     for j, v in enumerate(reversed(values[:i])):
         if v[0] == c:
             res.append(i-j-1)
@@ -108,47 +64,110 @@ def find_same_neighbour( i, c, values ):
             break
     return res
 
-def step( res ):
-    return res
+def find_move_min_cost( cid, pos, N, vec ):
+    # First char in right.
+    moves = [ ]
+    print( 'Cluster %s, pos %d, size %d' % (cid,pos,N) )
+    for i, v in enumerate(vec[pos+N+1:]):
+        if v[0] == cid:
+            moves.append( (pos+N+i+2, (pos+N)) )
+            break
+    for i, v in enumerate( reversed(vec[:pos-1]) ):
+        if v[0] == cid:
+            moves.append( (pos-i-1, pos)  )
+            break
 
-def recompute( values ):
+    res = [ (cost_of_moving_elem( vec[x[1]], x[0] ), *x) for x in moves ]
+    print( 'Cost', res )
+    return sorted(res)[0]
+
+def make_move( move, vec ):
+    cost, frm, to = move
+    print( '[DBEUG] Moving %d -> %d' % (frm,to) )
+    if frm < to:
+        vec.insert( to, vec[frm] )
+        del vec[frm]
+    else:
+        vec.insert( to, vec[frm])
+        del vec[frm+1]
+
+def find_cluster_with_max_len( vec ):
+    maxLen = max( [ x[2]['k'] for x in vec ] )
+    clusters, cluster, cid = [ ], [0,0], ''
+    clusterStart = False
+
+    for i, v in enumerate( vec ):
+        if not clusterStart and v[2]['k'] == maxLen:
+            clusterStart = True
+            cluster[0] = i
+        elif clusterStart and v[2]['k'] != maxLen:
+            cluster[1] = i
+            clusters.append( (v[0], cluster) )
+            cluster = [0,0]
+            clusterStart = False
+
+    for x,c in clusters:
+        assert vec[c[0]][0] == vec[c[1]][0], [vec[i] for i in c]
+    return maxLen, clusters
+
+def remove_cluster( vec, clusters ):
+    notId = [ ]
+    print( '[INFO] Removing %s' % str( clusters ) )
+    for cid, cluster in clusters:
+        notId += list( range( cluster[0], cluster[1] ) )
+    return [ x for i, x in enumerate(vec) if i not in notId ]
+
+def cluster_recurse( vec, k, res, num_call ):
+    if num_call == 5:
+        return res
+
+    # Find the strongest cluster.
+    recompute( vec, k )
+    maxLen, clusters = find_cluster_with_max_len( vec )
+    if (maxLen + 1) % k == 0:
+        # This is a good cluster. Remove it and keep it in res
+        res.append( clusters )
+        vec = remove_cluster( vec, clusters )
+        recompute( vec, k )
+        print_result( vec )
+        return cluster_recurse( vec, k, res, num_call + 1 )
+
+    # Move a char to this cluster.
+    print( 'CLUSTERS:', clusters )
+    print_result( vec, 3 )
+    for clusterId, cluster in clusters:
+        maxLen = cluster[1] - cluster[0]
+        fI = find_move_min_cost( clusterId, cluster[0], maxLen, vec )
+        if fI:
+            make_move( fI, vec )
+            recompute( vec, k )
+            print_result( vec )
+
+    return cluster_recurse( vec, k, res, num_call + 1 )
+
+def recompute( values, k ):
     for i, (c, l, attrib) in enumerate(values):
         alreadySeen = [ ]
         samec = find_same_neighbour(i, c, values)
-        attrib['k'] = len(samec)
+        attrib['k'] = len(samec) 
 
 def dynamic_programming( vec, k ):
     res = [ ]
     for i, (c,l) in enumerate( vec ):
-        res.append( (c, l, dict(k=0)) )
+        res.append( (c, l, dict(k=0,cid=0)) )
 
-    orig = res[:]
-    recompute( res )
+    recompute( res, k )
     print_result( res )
 
-    step( res )
-    print( cost( orig, res ) )
-    # Now do the step.
-
-
-def random_grouping( vec, n ):
-    for i, (v,oi) in enumerate( vec ):
-        xi = abs(i - oi)
-        print( xi, v, oi )
+    res = cluster_recurse( res, k, [ ], 0 )
 
 
 def cluster_data( vec, k, save = True ):
-    # Cluster the vector for size of chunk n.
-    # First we build the flow graph.
-    #g = build_flow_graph( vec, n )
-    #sol = compute_solution( g )
-    #if save:
-    #    write_graph( g, 'network.dot' )
     dynamic_programming( vec, k )
 
 def test( ):
     np.random.seed( 0 )
-    data = np.random.choice( list( 'ABCDE' ), 100, p = [0.2,0.2,0.2,0.1,0.3] )
+    data = np.random.choice( list( 'ABCDE' ), 120, p = [0.2,0.2,0.2,0.1,0.3] )
     data = list( zip( data, range(0,len(data) )))
     cluster_data( data, 3 )
 
