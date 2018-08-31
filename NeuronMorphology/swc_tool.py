5231594e19724ca969aca3e34416ca4447ad33fa
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-31 12:14:20 +0530

    Plotting is fine.

--- NeuronMorphology/swc_tool.py
+++ NeuronMorphology/swc_tool.py
@@ -17,6 +17,31 @@ import networkx as nx
 
 args_ = None
 
+def data_for_cylinder_along_z(center_x,center_y,radius,height_z):
+    z = np.linspace(0, height_z, 50)
+    theta = np.linspace(0, 2*np.pi, 50)
+    theta_grid, z_grid=np.meshgrid(theta, z)
+    x_grid = radius*np.cos(theta_grid) + center_x
+    y_grid = radius*np.sin(theta_grid) + center_y
+    return x_grid,y_grid,z_grid
+
+def plot_cylinder( p0, p1, R, ax ):
+    # This is from https://stackoverflow.com/a/32383775/1805129
+    v = p1 - p0
+    mag = np.linalg.norm(v)
+    v = v / mag
+    not_v = np.array([1, 0, 0])
+    if (v == not_v).all():
+        not_v = np.array([0, 1, 0])
+    n1 = np.cross(v, not_v)
+    n1 /= np.linalg.norm(n1)
+    n2 = np.cross(v, n1)
+    t = np.linspace(0, mag, 100)
+    theta = np.linspace(0, 2 * np.pi, 100)
+    t, theta = np.meshgrid(t, theta)
+    X, Y, Z = [p0[i] + v[i] * t + R * np.sin(theta) * n1[i] + R * np.cos(theta) * n2[i] for i in [0, 1, 2]]
+    ax.plot_surface(X, Y, Z)
+
 def parse_swc( swctxt ):
     global args_
     g = nx.DiGraph()
@@ -25,21 +50,37 @@ def parse_swc( swctxt ):
             continue
         if l[0] == '#':
             continue
-        fs = l.split()
+        fs = [ float(x) for x in l.split()]
         if not len(fs) == 7:
             print( "[WARN ] Bad line: Ignored %d: %s" % (i,l) )
             continue
 
         # id, type, x, y, z, radius, parent
-        g.add_node( fs[0], type=fs[1], x=fs[2], y=fs[3], z=fs[4], radius=fs[5] )
+        pos = fs[1:4]
+        #  print(pos)
+        g.add_node( fs[0], pos=pos, type=fs[1], x=fs[2], y=fs[3], z=fs[4], radius=fs[5] )
         g.add_edge( fs[6], fs[0] )
     return g
 
 def plot_graph( g ):
     global args_
+    import matplotlib as mpl
     import matplotlib.pyplot as plt
-    nx.draw_networkx( g )
-    plt.savefig( '%s.graph.png' % args_.swc )
+    mpl.style.use( 'ggplot' )
+    mpl.rcParams['axes.linewidth'] = 0.2
+    mpl.rcParams['lines.linewidth'] = 1.0
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.grid( False )
+    #  ax.axis( 'off' )
+    pos = nx.get_node_attributes( g, 'pos' ).values()
+    X, Y, Z = zip(*pos)
+    ax.scatter( X, Y, Z, s = 2 )
+    outfile = '%s.graph.png' % args_.swc
+    plt.suptitle( args_.swc )
+    plt.savefig( outfile )
+    print( 'Morphology is saved to %s' % outfile )
 
 def main( args ):
     global args_
@@ -48,8 +89,8 @@ def main( args ):
     with open( args_.swc, 'r' ) as f:
         txt = f.read()
     g = parse_swc( txt )
-    plot_graph( g )
-    
+    if args_.plot:
+        plot_graph( g )
 
 if __name__ == '__main__':
     import argparse
@@ -59,6 +100,11 @@ if __name__ == '__main__':
     parser.add_argument('--swc', '-i'
         , required = True
         , help = 'Input file'
+        )
+    parser.add_argument('--plot', '-p'
+        , required = False, action = 'store_true'
+        , help = 'Plot morphology using matplotlib (only rough morphology is '
+                ' plotted for quick overview.)'
         )
     class Args: pass 
     args = Args()
