d45a1483be47034d7c5a255fe62936fd3631c904
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-03 10:07:44 +0530

    Added module tinyrandom.

--- LinearAlgebra/tinyrandom.py
+++ LinearAlgebra/tinyrandom.py
@@ -1,17 +1,54 @@
-from __future__ import division, print_function
-import tinynumpy.tinynumpy as tnp
+# -*- coding: utf-8 -*-
+"""tinyrandom.py: 
+"""
+
+from __future__ import division
+from __future__ import absolute_import
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2018-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawar.s.rajput@gmail.com"
+__status__           = "Development"
+
 import itertools
 import random
 
+def seed( seed ):
+    random.seed(seed)
+
 def rand( *size ):
+    # Generate an array of given size. All elements are sampled between 0 and 1
     A = tnp.zeros( size )
     for s in itertools.product( *map(range, size) ):
         A[s] = random.random()
     return A
 
-def test( ):
-    a = rand(4,4,4)
+def randn( *size ):
+    # Generate an array of given size. All elements are sampled between 0 and 1
+    A = tnp.zeros( size )
+    for s in itertools.product( *map(range, size) ):
+        A[s] = random.gauss()
+    return A
 
-if __name__ == '__main__':
-    test()
+def randint( low, high = None, size=None ):
+    # Generate an array of given size. All elements are sampled between 0 and 1
+    if high is None:
+        high = low
+        low = 0
+    if size is None:
+        size = 1
+    A = tnp.zeros( size )
+    for s in itertools.product( *map(range, size) ):
+        A[s] = random.randint(low, high)
+    return A
+
+def random( size = None ):
+    if size is None:
+        size = 1
+    A = tnp.zeros( size )
+    for s in itertools.product( *map(range, size) ):
+        A[s] = random.random( )
+    return A
 
