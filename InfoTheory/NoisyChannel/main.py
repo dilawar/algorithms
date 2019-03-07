6907b3acc6b0fee365e391dae62e309bf4676f21
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-07 12:31:03 +0530

    Send over a channel.

---
+++ InfoTheory/NoisyChannel/main.py
@@ -0,0 +1,21 @@
+"""main.py: 
+Send an image over noisy channel.
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
