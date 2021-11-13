2dbd946969d39d17da7e72b25104888e8cdeec91
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-11-13 13:13:54 +0530

    Solve maze using smoldyn.

--- MazeGenerator/maze_solver_particles.py
+++ MazeGenerator/maze_solver_particles.py
@@ -3,24 +3,20 @@
 import math
 import smoldyn
 
-
 def get_axis(line):
     x0, y0, x1, y1 = line
     theta = math.atan2(y1 - y0, x1 - x0)
-    if math.isclose(theta, 0.0):
+    if math.isclose(theta, 0.0) or math.isclose(theta, math.pi):
         return '+y'
-    if math.isclose(theta, math.pi):
-        return '-y'
-    if math.isclose(theta, math.pi/2.0):
-        return '+x'
-    return '-x'
-
+    return '+x'
 
 def add_maze(s, mazefile):
     panels = []
+    offset = 1.5
+    scale = 10
     with open(mazefile, "r") as f:
         for i, l in enumerate(f.read().strip().split("\n")):
-            line = [10 * (1.5 + float(x)) for x in l.split()]
+            line = [scale * (offset + float(x)) for x in l.split()]
             axis = get_axis(line)
             x0, y0, x1, y1 = line
             r = s.addRectangle(
@@ -30,12 +26,20 @@ def add_maze(s, mazefile):
                 name=f"r{i}",
             )
             panels.append(r)
+    # close the entry notch
+    r = s.addRectangle(
+            corner=[10, 10],
+            dimensions=[30],
+            axis='+y',
+            name=f"rrr",
+            )
+    panels.append(r)
     return panels
 
 
-s = smoldyn.Simulation(low=[0, 0], high=[200, 200])
+s = smoldyn.Simulation(low=[0, 0], high=[120, 120])
 A = s.addSpecies("A", difc=2, color="blue")
-A.addToSolution(1000, pos=[10, 10])
+A.addToSolution(1000, pos=[15, 15])
 
 panels = add_maze(s, "_maze.txt")
 maze = s.addSurface("maze", panels=panels)
@@ -43,5 +47,5 @@ maze.setAction("front", [A], "reflect")
 maze.setAction("back", [A], "reflect")
 maze.setStyle("both", thickness=1)
 
-s.addGraphics("opengl", iter=10)
-s.run(100, dt=0.01)
+s.addGraphics("opengl", iter=30)
+s.run(3000, dt=0.01)
